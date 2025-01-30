6e50ff138f9e8ecbb64a852cb8b66ffc7b0f59bd
Azamat Qasimov <azamatqsimov@gmail.com>
2025-01-30 01:12:12 -0800

    new color updt

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ let changeImg = document.getElementById("changeImg");
 let btn = document.getElementById("btn");
 let lqo = document.getElementById("lqo");
 let ires = document.getElementById("ires");
+let bds = document.getElementById("bds");
 
 function redShou() {
   changeImg.src = "assest/pngwing 2.png";
   btn.style.backgroundColor = "#E10F15";
   lqo.style.color = "#E10F15";
   ires.style.color = "#C2C2C2";
+  bds.style.background = "linear-gradient(178deg, rgb(232 71 85 / 23%), white)";
 }
 
 function changeReds() {
@@ -18,12 +20,15 @@ function changeReds() {
   btn.style.backgroundColor = "#2C77B9";
   lqo.style.color = "#2C77B9";
   ires.style.color = "#F24E3A";
+  bds.style.background =
+    "linear-gradient(178deg, rgb(71 232 123 / 23%), white)";
 }
 function blackShou() {
   changeImg.src = "assest/pngwing 1.png";
   btn.style.backgroundColor = "#FB5014";
   lqo.style.color = "#FB5014";
   ires.style.color = "#17B4CC";
+  bds.style.background = "linear-gradient(178deg, rgb(71 134 232 / 23%),white)";
 }
 
 changeRed.addEventListener("mousedown", redShou);
